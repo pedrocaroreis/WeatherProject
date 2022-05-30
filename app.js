d531cd6a31aa38fc397377753b475d8e7d7424d3
Pedro Caro <pedrosreis@gmail.com>
2022-05-30 15:37:40 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const https =require("https");
+
+const app = express();
+
+app.get("/", function (res, res){
+
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=Brasilia,br&appid=9cc9e32c924be38747da62f005f00fe6&units=metric";
+
+  https.get(url, function (response){
+    console.log(response.statusCode);
+
+    response.on("data", function(data){
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const weatherDescription = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
+      const urlIcon = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+
+      res.write("<p>The weather in Brasilia is " + weatherDescription + ".</p>");
+      res.write("<h1>The temperature in Brasilia right now is " + temp + " Celsius degrees.</h1>");
+      res.write("<img src=" + urlIcon + ">");
+      res.send();
+    })
+  })
+})
+
+
+
+
+
+app.listen(3000, function (){
+  console.log("Server is running on port 3000.");
+})
